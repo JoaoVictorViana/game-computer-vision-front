ec5ee1b4901e9ae644f3d163c7b1d5865a88a5ae
João Victor Viana <jv.duarte.viana@gmail.com>
2023-11-15 20:59:56 -0300

    chore: eslint e prettier configurado

--- next.config.js
+++ next.config.js
@@ -1,4 +1,37 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      type: 'asset',
+      resourceQuery: /url/,
+    })
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        and: [/\.(js|ts)x?$/],
+      },
+      resourceQuery: { not: [/url/] },
+      use: ['@svgr/webpack'],
+    })
+
+    // config.module.rules.unshift({
+    //   test: /pdf\.worker\.(min\.)?js/,
+    //   use: [
+    //     {
+    //       loader: "file-loader",
+    //       options: {
+    //         name: "[contenthash].[ext]",
+    //         publicPath: "/_next/static/worker",
+    //         outputPath: "/static/worker"
+    //       }
+    //     }
+    //   ]
+    // });
+
+    return config
+  },
+}
 
 module.exports = nextConfig
